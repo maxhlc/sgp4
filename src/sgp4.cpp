bed9e4e09ca2e0c255967d40c11db50fecf291ee
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2023-04-18 12:40:31 +0100

    Remove unused code

--- src/sgp4.cpp
+++ src/sgp4.cpp
@@ -54,55 +54,8 @@
  *       ----------------------------------------------------------------      */
 
 #include <sgp4/sgp4.hpp>
-// errors are from the order of routines...
-// must be defined in order of use...
 
 #define pi 3.14159265358979323846
-
-// define global variables here, not in .h
-// use extern in main
-char help = 'n';
-FILE *dbgfile;
-
-/* ----------- local functions - only ever used internally by sgp4 ---------- */
-static void dpper(double e3, double ee2, double peo, double pgho, double pho, double pinco, double plo, double se2, double se3, double sgh2, double sgh3,
-                  double sgh4, double sh2, double sh3, double si2, double si3, double sl2, double sl3, double sl4, double t, double xgh2, double xgh3,
-                  double xgh4, double xh2, double xh3, double xi2, double xi3, double xl2, double xl3, double xl4, double zmol, double zmos, double inclo,
-                  char init, double &ep, double &inclp, double &nodep, double &argpp, double &mp, char opsmode);
-
-static void dscom(double epoch, double ep, double argpp, double tc, double inclp, double nodep, double np, double &snodm, double &cnodm, double &sinim,
-                  double &cosim, double &sinomm, double &cosomm, double &day, double &e3, double &ee2, double &em, double &emsq, double &gam, double &peo,
-                  double &pgho, double &pho, double &pinco, double &plo, double &rtemsq, double &se2, double &se3, double &sgh2, double &sgh3, double &sgh4,
-                  double &sh2, double &sh3, double &si2, double &si3, double &sl2, double &sl3, double &sl4, double &s1, double &s2, double &s3, double &s4,
-                  double &s5, double &s6, double &s7, double &ss1, double &ss2, double &ss3, double &ss4, double &ss5, double &ss6, double &ss7, double &sz1,
-                  double &sz2, double &sz3, double &sz11, double &sz12, double &sz13, double &sz21, double &sz22, double &sz23, double &sz31, double &sz32,
-                  double &sz33, double &xgh2, double &xgh3, double &xgh4, double &xh2, double &xh3, double &xi2, double &xi3, double &xl2, double &xl3,
-                  double &xl4, double &nm, double &z1, double &z2, double &z3, double &z11, double &z12, double &z13, double &z21, double &z22, double &z23,
-                  double &z31, double &z32, double &z33, double &zmol, double &zmos);
-
-static void dsinit(
-    // sgp4fix no longer needed pass in xke
-    // gravconsttype whichconst,
-    double xke, double cosim, double emsq, double argpo, double s1, double s2, double s3, double s4, double s5, double sinim, double ss1, double ss2,
-    double ss3, double ss4, double ss5, double sz1, double sz3, double sz11, double sz13, double sz21, double sz23, double sz31, double sz33, double t,
-    double tc, double gsto, double mo, double mdot, double no, double nodeo, double nodedot, double xpidot, double z1, double z3, double z11, double z13,
-    double z21, double z23, double z31, double z33, double ecco, double eccsq, double &em, double &argpm, double &inclm, double &mm, double &nm, double &nodem,
-    int &irez, double &atime, double &d2201, double &d2211, double &d3210, double &d3222, double &d4410, double &d4422, double &d5220, double &d5232,
-    double &d5421, double &d5433, double &dedt, double &didt, double &dmdt, double &dndt, double &dnodt, double &domdt, double &del1, double &del2,
-    double &del3, double &xfact, double &xlamo, double &xli, double &xni);
-
-static void dspace(int irez, double d2201, double d2211, double d3210, double d3222, double d4410, double d4422, double d5220, double d5232, double d5421,
-                   double d5433, double dedt, double del1, double del2, double del3, double didt, double dmdt, double dnodt, double domdt, double argpo,
-                   double argpdot, double t, double tc, double gsto, double xfact, double xlamo, double no, double &atime, double &em, double &argpm,
-                   double &inclm, double &xli, double &mm, double &xni, double &nodem, double &dndt, double &nm);
-
-static void initl(
-    // not needeed. included in satrec if needed later
-    // int satn,
-    // sgp4fix assin xke and j2
-    // gravconsttype whichconst,
-    double xke, double j2, double ecco, double epoch, double inclo, double &no, char &method, double &ainv, double &ao, double &con41, double &con42,
-    double &cosio, double &cosio2, double &eccsq, double &omeosq, double &posq, double &rp, double &rteosq, double &sinio, double &gsto, char opsmode);
 
 namespace sgp4::SGP4Funcs {
 
@@ -270,11 +223,13 @@ namespace sgp4::SGP4Funcs {
                 // nodep used without a trigonometric function ahead
                 if ((nodep < 0.0) && (opsmode == 'a'))
                     nodep = nodep + twopi;
-                if (fabs(xnoh - nodep) > pi)
-                    if (nodep < xnoh)
+                if (fabs(xnoh - nodep) > pi) {
+                    if (nodep < xnoh) {
                         nodep = nodep + twopi;
-                    else
+                    } else {
                         nodep = nodep - twopi;
+                    }
+                }
                 mp = mp + pl;
                 argpp = xls - mp - cosip * nodep;
             }
@@ -882,7 +837,7 @@ namespace sgp4::SGP4Funcs {
                        double argpdot, double t, double tc, double gsto, double xfact, double xlamo, double no, double &atime, double &em, double &argpm,
                        double &inclm, double &xli, double &mm, double &xni, double &nodem, double &dndt, double &nm) {
         const double twopi = 2.0 * pi;
-        int iretn, iret;
+        int iretn;
         double delt, ft, theta, x2li, x2omi, xl, xldot, xnddt, xndt, xomi, g22, g32, g44, g52, g54, fasx2, fasx4, fasx6, rptim, step2, stepn, stepp;
 
         fasx2 = 0.13130908;
@@ -939,7 +894,6 @@ namespace sgp4::SGP4Funcs {
                 delt = stepn;
 
             iretn = 381; // added for do loop
-            iret = 0;    // added for loop
             while (iretn == 381) {
                 /* ------------------- dot terms calculated ------------- */
                 /* ----------- near - synchronous resonance terms ------- */
@@ -967,7 +921,6 @@ namespace sgp4::SGP4Funcs {
                 /* ----------------------- integrator ------------------- */
                 // sgp4fix move end checks to end of routine
                 if (fabs(t - atime) >= stepp) {
-                    iret = 0;
                     iretn = 381;
                 } else // exit here
                 {
